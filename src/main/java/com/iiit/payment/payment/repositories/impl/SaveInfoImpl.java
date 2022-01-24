d9663a74a4ba79eab4b12e0f258b972932e5f061
thaminduheiyanthuduwa <thaminduanu2@gmail.com>
2022-01-24 09:53:31 +0530

    Payment implementation

--- src/main/java/com/iiit/payment/payment/repositories/impl/SaveInfoImpl.java
+++ src/main/java/com/iiit/payment/payment/repositories/impl/SaveInfoImpl.java
@@ -6,6 +6,7 @@ import com.iiit.payment.payment.model.PaymentObj;
 import com.iiit.payment.payment.repositories.SaveInfo;
 import com.iiit.payment.payment.transation.Expense;
 import com.iiit.payment.payment.transation.Income;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -30,59 +31,54 @@ public class SaveInfoImpl implements SaveInfo {
     @Override
     public void saveCategoryDetails(List<Category> categories) throws IOException {
 
-        if (!categories.isEmpty()) {
-            FileOutputStream fileOutputStream = new FileOutputStream("category.txt");//save file to write objects
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            for (Category categoryTemp : categories) {
-                objectOutputStream.writeObject(categoryTemp);
-            }
-            objectOutputStream.close();
-            System.out.println("Successfully Saved");
+        FileOutputStream fileOutputStream = new FileOutputStream("category.txt");//save file to write objects
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        for (Category categoryTemp : categories) {
+            objectOutputStream.writeObject(categoryTemp);
         }
+        objectOutputStream.close();
+        System.out.println("Successfully Saved");
+
     }
 
     @Override
     public void saveIncome(List<Income> transactions) throws IOException {
 
-        if (!transactions.isEmpty()) {
-            FileOutputStream fileOutputStream = new FileOutputStream("income.txt");//save file to write objects
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            for (Income transaction : transactions) {
-                objectOutputStream.writeObject(transaction);
-            }
-            objectOutputStream.close();
-            System.out.println("Successfully Saved");
+        FileOutputStream fileOutputStream = new FileOutputStream("income.txt");//save file to write objects
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        for (Income transaction : transactions) {
+            objectOutputStream.writeObject(transaction);
         }
+        objectOutputStream.close();
+        System.out.println("Successfully Saved");
+
     }
 
     @Override
     public void saveExpenses(List<Expense> transactions) throws IOException {
 
-        if (!transactions.isEmpty()) {
-            FileOutputStream fileOutputStream = new FileOutputStream("expenses.txt");//save file to write objects
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            for (Expense transaction : transactions) {
-                objectOutputStream.writeObject(transaction);
-            }
-            objectOutputStream.close();
-            System.out.println("Successfully Saved");
+        FileOutputStream fileOutputStream = new FileOutputStream("expenses.txt");//save file to write objects
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        for (Expense transaction : transactions) {
+            objectOutputStream.writeObject(transaction);
         }
-    }
+        objectOutputStream.close();
+        System.out.println("Successfully Saved");
 
+    }
 
 
     @Override
     public void saveBudgetDetails(List<PaymentObj> budgets) throws IOException {
 
-        if (!budgets.isEmpty()) {
-            FileOutputStream fileOutputStream = new FileOutputStream("budget.txt");//save file to write objects
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            for (PaymentObj budget : budgets) {
-                objectOutputStream.writeObject(budget);
-            }
-            objectOutputStream.close();
-            System.out.println("Successfully Saved");
+
+        FileOutputStream fileOutputStream = new FileOutputStream("budget.txt");//save file to write objects
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        for (PaymentObj budget : budgets) {
+            objectOutputStream.writeObject(budget);
         }
+        objectOutputStream.close();
+        System.out.println("Successfully Saved");
 
     }
 
